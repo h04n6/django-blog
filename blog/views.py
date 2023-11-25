0b32a67a68b7f6c2f34489ec23418d30d382afe4
hoanghd <haduchoang@cryptopie-labo.com>
2023-11-25 23:44:01 +0700

    add similar post by tag in detail template

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from django.db.models import Count
+
 from taggit.models import Tag
 
 from blog.form import EmailPostForm, CommentForm
@@ -62,11 +64,17 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts })
 
 
 def post_share(request, post_id):

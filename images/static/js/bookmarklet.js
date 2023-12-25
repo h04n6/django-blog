6562bddf5201d91301fe8e6c83f9c145d05e1b0d
hoanghd <haduchoang@cryptopie-labo.com>
2023-12-25 15:36:26 +0700

    building a boolmarklet js

---
+++ images/static/js/bookmarklet.js
@@ -0,0 +1,44 @@
+const siteUrl = '//127.0.0.1:8000/';
+const styleUrl = siteUrl + 'static/css/bookmarklet.css';
+const minWidth = 250;
+const minHeight = 250;
+
+// load CSS
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
+head.appendChild(link);
+
+// load HTML
+var body = document.getElementsByTagName('body')[0];
+boxHtml = '<div id="bookmarklet">' +
+    '<a href="#" id="close">&times;</a>' +
+    '<h1>Select an image to bookmark:</h1>' +
+    '<div class="images"></div>' +
+    '</div>';
+body.innerHTML += boxHtml;
+
+function bookmarkletLaunch() {
+    bookmarklet = document.getElementById('bookmarklet');
+    let imagesFound = bookmarklet.querySelector('.images');
+
+    // clear images found
+    imagesFound.innerHTML = '';
+    // display bookmarklet
+    bookmarklet.style.display = 'block';
+
+    // close event
+    bookmarklet.querySelector('#close')
+        .addEventListener('click', function() {
+            bookmarklet.style.display = 'none'
+        });
+
+    // find images in the DOM with the minimum dimensions
+    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+
+}
+
+// launch the bookmarklet
+bookmarkletLaunch();
